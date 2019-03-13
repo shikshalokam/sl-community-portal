e8e5b43464fc8b2ae1e091010c54115ddca30a68
sushmita <sushmita@tunerlabs.com>
2019-03-13 15:31:00 +0530

    added footer, icon and login button to sl-community-portal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { TranslateService } from './core/services/translate-service/translate.service';
+import { TranslateService, GlobalConfigurationService } from 'shikshalokam';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from './modules/private-modules/auth-service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +10,75 @@ import { TranslateService } from './core/services/translate-service/translate.se
 })
 export class AppComponent {
 
-  constructor(private translate: TranslateService) {
-    translate.use('en').then(() => {
+  // constructor(private translate: TranslateService) {
+  //   translate.use('en').then(() => {
     
-    });
-  }
+  //   });
+  // }
 
-  links = [  
-          { 
-            linkHeading : "headings.features",
-            options:[ 
+  // links = [  
+  //         { 
+  //           linkHeading : "headings.features",
+  //           options:[ 
                       
                       
-                    ]  
-            }
-        ] ;
+  //                   ]  
+  //           }
+  //       ] ;
+
+  isLoggedIn:boolean;
+  programId;
+  assessmentId;
+  // links ;
+  opened = true;
+  pushMode = 'side';
+  currentUser;
+  logo =" ./assets/shikshalokam.png";
+  roleAcess=[];
+  constructor(private translate: TranslateService,private route : ActivatedRoute,private authService :AuthService , private globalConfigService:GlobalConfigurationService) {
+    translate.use('en').then(() => {
+      
+    });
+    if (window.screen.width < 760) { // 768px portrait
+      this.opened = false;
+      this.pushMode = 'push';
+    }
+    this.currentUser = this.authService.getCurrentUserDetails();
+    if(this.currentUser){
+      this.isLoggedIn = true;
+    }else{
+      this.isLoggedIn = false;
+      // this.currentUser = "blank"
+    }
+    console.log(this.isLoggedIn)
+   }
+
+  ngOnInit() {
+    
+  }
+   
+  onLogout(){
+    this.authService.getLogout();
+    this.onLogin();
+  } 
+  onLogin(){
+    // this.authService.init({onload:'login-required'});
+    console.log("login Called")
+    this.authService.getLogin();
+  }
+  onResize(event)
+  {
+    if(event.target.innerWidth < 760)
+    {
+      this.opened = false;
+      this.pushMode = 'push';
+    }
+    else{
+      this.opened = true;
+      this.pushMode = 'side';
+
+    }
+  }
       
 }
 
