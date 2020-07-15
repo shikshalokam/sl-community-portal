69590d3865f0fb3b0418550bb26c346ac0a5f235
srikanth <srikanth.g@pacewisdom.com>
2020-07-15 10:18:43 +0530

    removed media queries with bootstrap 4

--- src/app/modules/library/learning-resources/learning-resources.component.ts
+++ src/app/modules/library/learning-resources/learning-resources.component.ts
@@ -12,7 +12,65 @@ import { environment } from 'src/environments/environment';
 })
 export class LearningResourcesComponent implements OnInit {
   filtersData: any;
-  resourceData: any;
+  resourceData: any = [
+    {
+      "title": "Recently Added",
+      "type": "card",
+      "imageUrl": "",
+      "description": "Recently added",
+      "viewMoreUrl": "https://dev.shikshalokam.org",
+      "totalCount": 4932,
+      "resources": [
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_2127561039911895041422",
+          "title": " AWS - 10,000 Foot Overview (2019 course)",
+          "description": "Enter description for Resource",
+          "time": "2019-05-06T11:20:00.486+0000"
+        },
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_2127567452069724161593",
+          "title": " Snowball Lab",
+          "description": "Enter description for Resource",
+          "time": "2019-05-07T09:04:33.897+0000"
+        },
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_21258040361213952015738",
+          "appIcon": "https://ekstep-public-qa.s3-ap-south-1.amazonaws.com/content/do_21258040361213952015738/artifact/download-3_1531474695189.thumb.jpg",
+          "title": "!@#$%^&*()_+",
+          "description": "",
+          "time": "2018-08-31T05:36:40.308+0000"
+        }
+      ]
+    },
+    {
+      "title": "Most Popular",
+      "type": "card",
+      "description": "Most popular",
+      "totalCount": 4932,
+      "viewMoreurl": "https://dev.shikshalokam.org",
+      "resources": [
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_2127561039911895041422",
+          "title": " AWS - 10,000 Foot Overview (2019 course)",
+          "description": "Enter description for Resource",
+          "time": "2019-05-06T11:20:00.486+0000"
+        },
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_2127567452069724161593",
+          "title": " Snowball Lab",
+          "description": "Enter description for Resource",
+          "time": "2019-05-07T09:04:33.897+0000"
+        },
+        {
+          "url": "https://dev.shikshalokam.org/resources/play/content/do_21258040361213952015738",
+          "appIcon": "https://ekstep-public-qa.s3-ap-south-1.amazonaws.com/content/do_21258040361213952015738/artifact/download-3_1531474695189.thumb.jpg",
+          "title": "!@#$%^&*()_+",
+          "description": "",
+          "time": "2018-08-31T05:36:40.308+0000"
+        }
+      ]
+    }
+  ];
 
   constructor(private communityService: CommunityService) { }
 
